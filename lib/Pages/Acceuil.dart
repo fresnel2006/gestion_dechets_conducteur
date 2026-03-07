191937a693f4bef156b8746fb2a47c1a526e7437
unknown <traoreangefresnel@gmail.com>
2026-03-07 13:36:39 +0000

    ajouter

--- lib/Pages/Acceuil.dart
+++ lib/Pages/Acceuil.dart
@@ -69,7 +69,7 @@ class _AcceuilPageState extends State<AcceuilPage> {
       return;
     }
 
-    final String accessToken = "pk.eyJ1IjoiZnJlc25lbDYwNyIsImEiOiJjbWhrbGx1MzMwOGV4MmtxazdsOWp0dzIxIn0.v02HfvuS1iZnm_-od_niSw";
+    final String accessToken = "";
 
     final url = Uri.parse(
         "https://api.mapbox.com/directions/v5/mapbox/driving/$longitude,$latitude;$yopougonLon,$yopougonLat?geometries=geojson&access_token=$accessToken");
